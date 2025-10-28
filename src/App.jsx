b7ed0e1917b18345f38aecbf194b2a25caddf3cf
Upasara <mihirangaupasara2000@gmail.com>
2025-10-28 16:58:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+ return (
+  <>
+   <div className='p-10 min-h-screen bg-amber-300'>
+    <div className='text-2xl flex items-center justify-center bg-amber-50'>
+     COMING SOON
+    </div>
+   </div>
+  </>
+ );
+}
+
+export default App;
